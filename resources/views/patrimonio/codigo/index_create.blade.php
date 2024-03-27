c706840652ccfdfb7e788dbbf8c49b9b10a89a98
AnaliceBattisti <analicebattistii@gmail.com>
2024-03-27 00:30:10 -0300

    Ajeitando view de codigo

--- resources/views/patrimonio/codigo/index_create.blade.php
+++ resources/views/patrimonio/codigo/index_create.blade.php
@@ -1,51 +1,73 @@
 @extends('layouts.app')
 @section('content')
-    @include('layouts.components.header', ['page_title' => 'Códigos do Patrimônio '. $patrimonio->nome, 'back' => true])
-    <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal" style="float: right">
-        <img src="{{URL::asset('/assets/plus.svg')}}" width="40px" alt="Icon de remoção">
-    </button>
-    <div class="row">
-        @foreach($patrimonio->codigos as $index => $codigo)
 
-            <div class="col-4 mt-2">
-                <label for="codigo">Código {{$index+1}}:</label>
-                <input class="form-control @error('codigo') is-invalid @enderror" id="codigo" type="text"
-                       name="codigo" @if(isset($codigo->codigo)) value="{{$codigo->codigo}}"
-                       @endif required
-                       autocomplete="codigo" autofocus>
-                @error('codigo')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-                <a href="{{route('patrimonio.codigo.delete', ['codigo_id' => $codigo->id])}}">Deletar</a>
-            </div>
+@push('styles')
+        <link rel="stylesheet" href="/css/modal.css">
+@endpush
 
-        @endforeach
-        @empty($patrimonio->codigos[0])
-            <p>Não há códigos cadastrados ainda.</p>
-        @endempty
-
+<div class="card mx-auto my-5 p-4" style="max-width: 600px;">
+    <div class="d-flex align-items-center mb-2 mt-3">
+        @include('layouts.components.header', ['page_title' => 'Códigos do Patrimônio '. $patrimonio->nome, 'back' => route('patrimonio.index')])
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal"
+            style="background-color: transparent; border: none; margin-bottom: 8px;">
+            <img src="{{ asset('assets/plus-circle-fill.svg') }}" alt="Ícone de Adição"
+                style="width: 30px; height: 30px;">
+        </button>
     </div>
+    <div class="card-body">
+        <div class="row">
+            @foreach($patrimonio->codigos as $index => $codigo)
+            <div class="col-6 mb-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Código {{$index+1}}</h5>
+                        <div class="d-flex align-items-center">
+                            <input class="form-control @error('codigo') is-invalid @enderror" type="text"
+                                name="codigo" @if(isset($codigo->codigo)) value="{{$codigo->codigo}}" @endif required
+                                autocomplete="codigo" autofocus>
+                            <a href="{{route('patrimonio.codigo.delete', ['codigo_id' => $codigo->id])}}"
+                                class="btn ms-2"><img src="{{ asset('/images/delete.png') }}" alt="Excluir"
+                                    style="width: 20px; height: 20px;"></a>
+                        </div>
+                        @error('codigo')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+                </div>
+            </div>
+            @endforeach
 
+            @empty($patrimonio->codigos[0])
+            <div class="col-12">
+                <div class="alert" role="alert">
+                    Não há códigos cadastrados ainda.
+                </div>
+            </div>
+            @endempty
+        </div>
+    </div>
+</div>
 
-    <!-- Modal -->
     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
          aria-hidden="true">
         <div class="modal-dialog">
-            <div class="modal-content">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content custom-modal bg-light">
+                    <div class="modal-header-predio">
+                        <a href="#" data-bs-dismiss="modal" aria-label="Fechar">
+                            <img src="{{ asset('assets/back.svg') }}" alt="Voltar">
+                        </a>
+                        <h5 class="modal-title mx-auto" id="exampleModalLabel">Adicionar Código ao Patrimônio</h5>
+                    </div>
+            <div class="modal-body">
                 <form method="POST" action="{{route('patrimonio.codigo.store')}}" enctype="multipart/form-data">
                     @csrf
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Adicionar Código ao Patrimônio</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
                     <div class="modal-body">
                         @include('patrimonio.codigo.form')
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-                        <button type="submit" class="btn btn-success">Adicionar</button>
+                    <div class="text-center mt-3">
+                        <button type="submit" class="btn btn-primary">Adicionar</button>
                     </div>
                 </form>
             </div>
