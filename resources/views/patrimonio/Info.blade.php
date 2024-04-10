8ebb19607b6d2d9417747101f269752b43ba4ac7
AnaliceBattisti <analicebattistii@gmail.com>
2024-04-09 22:12:56 -0300

    Info de Patrimonio +completo

--- resources/views/patrimonio/Info.blade.php
+++ resources/views/patrimonio/Info.blade.php
@@ -7,10 +7,29 @@
         @include('layouts.components.header', ['page_title' => 'Detalhes do Patrimônio', 'back' => true])
         <div class="card-body">
             <div class="modal-body">
-                <p><strong>Nome do Patrimônio:</strong> <span id="patrimonio_nome">{{ $patrimonio->nome }}</span></p>
-                <p><strong>Classificação:</strong> <span id="classificacao">{{ $classificacao->nome }}</span></p>
-                <p><strong>Valor Residual:</strong> <span id="valor_residual">R$ {{ number_format($classificacao->residual, 2, ',', '.') }}</span></p>
-                <p><strong>Vida Útil:</strong> <span id="vida_util">{{ $classificacao->vida_util }} meses</span></p>
+                <div class="d-flex justify-content-center">
+                    <div class="col-md-6">
+                        <p><strong>Nome do Patrimônio:</strong> <span id="patrimonio_nome">{{ $patrimonio->nome }}</span></p>
+                        <p><strong>Servidor:</strong> <span id="servidor">{{ $patrimonio->servidor->user->name }}</span></p>
+                        <p><strong>Nota Fiscal:</strong> <span id="nota_fiscal">{{ $patrimonio->nota_fiscal }}</span></p>
+                        <p><strong>Descrição:</strong> <span id="descricao">{{ $patrimonio->descricao }}</span></p>
+                        <p><strong>Valor:</strong> <span id="valor">R$ {{ number_format($patrimonio->valor, 2, ',', '.') }}</span></p>
+                        <p><strong>Data de Compra:</strong> <span id="data_compra">{{ $patrimonio->data_compra }}</span></p>
+                        <p><strong>Data de Incorporação:</strong> <span id="data_incorporacao">{{ $patrimonio->data_incorporação }}</span></p>
+                    </div>
+                    <div class="col-md-6">
+                                <p><strong>Localização:</strong></p>
+                                <p>
+                                    <strong>Setor:</strong> {{ $patrimonio->setor->nome ?? 'Não especificado' }}
+                                </p>
+                                <p>
+                                    <strong>Prédio:</strong> {{ $patrimonio->sala->predio->nome ?? 'Não especificado' }}
+                                </p>
+                                <p>
+                                    <strong>Sala:</strong> {{ $patrimonio->sala->nome ?? 'Não especificado' }}
+                                </p>
+                    </div>
+                </div>
                 <hr>
                 @php
                     // Calcular depreciação mensal
@@ -25,7 +44,7 @@
                     $depreciacaoAtual = $diferencaMeses * $depreciacaoMensal;
                     $valorAtual = $patrimonio->valor - $depreciacaoAtual;
                 @endphp
-                
+                <h5> Detalhe Contábil </h5>
                 <p><strong>Meses de Depreciação:</strong> <span id="meses">{{ $diferencaMeses }} meses</span></p>
                 <p><strong>Valor Inicial do Item:</strong> R$ <span id="valor_inicial">{{ number_format($patrimonio->valor, 2, ',', '.') }}</span></p>
                 <p><strong>Depreciação Atual do Item:</strong> R$ <span id="depreciacao_atual">{{ number_format($depreciacaoAtual, 2, ',', '.') }}</span></p>
