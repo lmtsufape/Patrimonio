82ba5807e360146751a9be88a50e3b4731e64875
Rener1 <rener.tome123@gmail.com>
2024-05-17 16:53:40 -0300

    Aumentado o volume de patrimônios criados pelo seeder. Todas as relações são aleatórias agora. Novos campos adicionados.

--- database/factories/PatrimonioFactory.php
+++ database/factories/PatrimonioFactory.php
@@ -2,33 +2,45 @@
 
 namespace Database\Factories;
 
+use App\Models\{User, Subgrupo, Situacao, Origem, Sala, UnidadeAdministrativa};
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Patrimonio>
- */
 class PatrimonioFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition()
     {
+        static $userIds = null;
+        static $subgrupoIds = null;
+        static $situacaoIds = null;
+        static $origemIds = null;
+        static $salaIds = null;
+        static $unidadeAdminIds = null;
+
+        if (is_null($userIds)) {
+            $userIds = User::pluck('id')->toArray();
+            $subgrupoIds = Subgrupo::pluck('id')->toArray();
+            $situacaoIds = Situacao::pluck('id')->toArray();
+            $origemIds = Origem::pluck('id')->toArray();
+            $salaIds = Sala::pluck('id')->toArray();
+            $unidadeAdminIds = UnidadeAdministrativa::pluck('id')->toArray();
+        }
+
         return [
             'nome' => $this->faker->streetName,
+            'nota_fiscal' => Str::random(10),
             'descricao' => $this->faker->sentence,
             'data_compra' => $this->faker->date,
-            'valor' => $this->faker->numberBetween($min = 100, $max = 5000),
+            'observacao' => $this->faker->text(),
+            'valor' => $this->faker->numberBetween(100, 5000),
             'empenho' => $this->faker->sentence,
             'conta_contabil' => $this->faker->sentence,
-            'user_id' => 2,
-            'subgrupo_id' => 1,
-            'situacao_id' => 1,
-            'origem_id' => 1,
-            'sala_id' => 1,
-            'unidade_admin_id' => 1
+            'user_id' => $this->faker->randomElement($userIds),
+            'subgrupo_id' => $this->faker->randomElement($subgrupoIds),
+            'situacao_id' => $this->faker->randomElement($situacaoIds),
+            'origem_id' => $this->faker->randomElement($origemIds),
+            'sala_id' => $this->faker->randomElement($salaIds),
+            'unidade_admin_id' => $this->faker->randomElement($unidadeAdminIds),
         ];
     }
 }
--- database/seeders/PatrimonioSeeder.php
+++ database/seeders/PatrimonioSeeder.php
@@ -16,8 +16,6 @@ class PatrimonioSeeder extends Seeder
      */
     public function run()
     {
-        Patrimonio::factory(2)->create();
-        Codigo::factory(2)->create(['patrimonio_id' => 1]);
-        Codigo::factory(1)->create(['patrimonio_id' => 2]);
+        Patrimonio::factory(30)->create();
     }
 }
